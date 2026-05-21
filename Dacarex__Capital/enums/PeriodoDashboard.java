56e96606ecf9e94a00e6477cc13fd870e26f675a
Daniel Copete <dcopjim3009g.educaand.es>
2026-05-21 10:35:08 +0200

    Añadiendo la estructura del proyecto

---
+++ Dacarex__Capital/enums/PeriodoDashboard.java
@@ -0,0 +1,16 @@
+package com.dacarex.capital.enums;
+
+public enum PeriodoDashboard {
+    MES("Mes"),
+    TRIMESTRE("Trimestre"),
+    ANIO("Año");
+
+    private final String valor;
+
+    PeriodoDashboard(String valor) { this.valor = valor; }
+
+    public String getValor() { return valor; }
+
+    @Override
+    public String toString() { return valor; }
+}
